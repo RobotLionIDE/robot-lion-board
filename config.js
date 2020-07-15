a54f75282c87995b930ee2d35ffa75479696bed2
RobotLionIDE <kozlov.gv@robot-lion.ru>
2020-07-15 12:25:33 +0300

    gpio math

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {    
     name : 'robot-lion',
     platform : 'arduino-esp32',
-    title : 'Robot Lion',
+    title : 'Robot Lion (ESP32)',
     description : 'Robot Lion (Compatible with all ESP32 board)',
     author : 'GriPower',
     website : 'https://robot-lion.ru',
     email : 'info@robot-lion.ru',
-    git : 'https://github.com/RobotLionIDE/robot-lion-board',
+    git : 'https://github.com/RobotLionIDE/robot-lion-board/',
     image : '/static/display.jpg',
     version : '1.0.1'
 };
